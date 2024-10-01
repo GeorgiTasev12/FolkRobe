dc3871ed4ee4fefc769977d16c0c7118800e4f91
Georgi Tasev <georgi.tasev12@gmail.com>
2024-10-01 17:10:22 +0300

    Added a new costumes page where it displays a list of costumes using the model.

---
+++ lib/ui/costumes_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:folk_robe/models/costume.dart';
+import 'package:folk_robe/ui/widgets/costume_item.dart';
+
+class CostumesPage extends StatelessWidget {
+  const CostumesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Costume> costumes = [
+      Costume(title: 'Шопски'),
+      Costume(title: 'Тракийски'),
+      Costume(title: 'Северняшки'),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back),
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: ListView.separated(
+          itemBuilder: (context, index) =>
+              CostumeItem(title: costumes[index].title),
+          separatorBuilder: (context, index) => const SizedBox(height: 5),
+          itemCount: costumes.length),
+    );
+  }
+}
