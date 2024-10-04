56a7f9f525cce8a0a8d9b8423b7cd8562dcccf90
Georgi Tasev <georgi.tasev12@gmail.com>
2024-10-04 12:21:21 +0300

    Update on gender card test, implemented the onTap callback for testing the onTap function, using tap and pump.

--- test/gender_card_test.dart
+++ test/gender_card_test.dart
@@ -5,23 +5,30 @@ import 'package:folk_robe/ui/widgets/gender_card.dart';
 void main() {
   testWidgets(
     'Display the card about selecting a gender',
-        (WidgetTester tester) async {
+    (WidgetTester tester) async {
       String title = 'Male';
       IconData icon = Icons.male_rounded;
+      bool onTapCallback = false;
 
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Scaffold(
-            body: GenderCard(
-              title: title,
-              icon: icon,
-            ),
+      void onTap() => onTapCallback = true;
+
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: GenderCard(
+            title: title,
+            icon: icon,
+            onTap: onTap,
           ),
-        )
-      );
+        ),
+      ));
 
       expect(find.text(title), findsOneWidget);
       expect(find.byIcon(icon), findsOneWidget);
+
+      await tester.tap(find.byType(GestureDetector));
+      await tester.pump();
+
+      expect(onTapCallback, true);
     },
   );
 }
