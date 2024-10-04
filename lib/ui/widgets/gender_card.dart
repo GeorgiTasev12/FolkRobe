347d6c7094a78cf11c535a61ead2daae70274912
Georgi Tasev <georgi.tasev12@gmail.com>
2024-10-04 12:20:24 +0300

    Update on the Gender page and card, implemented the onTap functionality for navigation.

--- lib/ui/widgets/gender_card.dart
+++ lib/ui/widgets/gender_card.dart
@@ -4,39 +4,44 @@ import 'package:folk_robe/constants.dart';
 class GenderCard extends StatelessWidget {
   final String title;
   final IconData? icon;
+  final void Function()? onTap;
 
   const GenderCard({
     super.key,
     required this.title,
     required this.icon,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 180,
-      height: 180,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(Constants.circularRadius),
-        ),
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              icon,
-              size: 70,
-            ),
-            const SizedBox(height: Constants.sizedBoxHeight),
-            Text(
-              title,
-              style: const TextStyle(
-                fontSize: Constants.titleFontSize,
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: 180,
+        height: 180,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(Constants.circularRadius),
+          ),
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                size: 70,
               ),
-            ),
-          ],
+              const SizedBox(height: Constants.sizedBoxHeight),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: Constants.titleFontSize,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
