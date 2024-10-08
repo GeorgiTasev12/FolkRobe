25fc6bf3fc9d66dba7acd02b80675a059b44665c
Georgi Tasev <georgi.tasev12@gmail.com>
2024-10-08 14:51:51 +0300

    Update on main.dart, to include the provider all the way from the entry point.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:folk_robe/constants.dart';
+import 'package:folk_robe/providers/costumesList_provider.dart';
 import 'package:folk_robe/ui/costume_list_page.dart';
 import 'package:folk_robe/ui/costumes_page.dart';
 import 'package:folk_robe/ui/gender_page.dart';
 import 'package:folk_robe/ui/home_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,22 +14,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        Constants.initialRouteString: (context) => const HomePage(),
-        Constants.costumesPageRouteString: (context) => const CostumesPage(),
-        Constants.genderPageRouteString: (context) => const GenderPage(),
-        Constants.costumesListPageRouteString: (context) => const CostumeListPage(),
-      },
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => CostumesListProvider(),
+      child: MaterialApp(
+        routes: {
+          Constants.initialRouteString: (context) => const HomePage(),
+          Constants.costumesPageRouteString: (context) => const CostumesPage(),
+          Constants.genderPageRouteString: (context) => const GenderPage(),
+          Constants.costumesListPageRouteString: (context) => const CostumeListPage(),
+        },
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: Constants.initialRouteString,
       ),
-      initialRoute: Constants.initialRouteString,
     );
   }
 }
