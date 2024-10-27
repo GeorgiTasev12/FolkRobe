9f1c3ab7b888deb15505491d5d2f934b2829dc1b
Georgi Tasev <georgi.tasev12@gmail.com>
2024-10-27 19:36:52 +0200

    Update on the model and created a database service.

--- lib/models/costume.dart
+++ lib/models/costume.dart
@@ -1,17 +1,23 @@
-class Costumes {
-  final List<Costume> costumes;
-
-  const Costumes({
-    this.costumes = const [],
-  });
-}
-
 class Costume {
-  int? id = 0;
-  String title;
+  final int? id;
+  final String title;
 
   Costume({
     this.id,
     required this.title,
   });
+
+  factory Costume.fromMap(Map<String, dynamic> map) {
+    return Costume(
+      id: map['id'] as int,
+      title: map['title'] as String,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+    };
+  }
 }
