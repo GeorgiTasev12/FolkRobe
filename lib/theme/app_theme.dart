4fc5f2eee11cb5a34bf6e56262c46f0c985ef77a
Georgi Tasev <georgi.tasev12@gmail.com>
2025-04-12 21:23:14 +0300

    Creating and setup a colors and styles for theme extension.

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:folk_robe/theme/styles/colors_and_styles.dart';
+
+class AppTheme extends ThemeExtension<AppTheme> {
+  final AppColors colors;
+  final AppTextStyles textStyles;
+
+  const AppTheme({
+    required this.colors,
+    this.textStyles = const AppTextStyles(),
+  });
+
+  @override
+  AppTheme copyWith({
+    AppColors? colors,
+    AppTextStyles? textStyles,
+  }) {
+    return AppTheme(
+      colors: colors ?? this.colors,
+      textStyles: textStyles ?? this.textStyles,
+    );
+  }
+
+  @override
+  AppTheme lerp(ThemeExtension<AppTheme>? other, double t) {
+    if (other is! AppTheme) return this;
+
+    return AppTheme(
+      colors: AppColors.lerp(colors, other.colors, t),
+    );
+  }
+
+  static ThemeData light() {
+    return ThemeData(
+      brightness: Brightness.light,
+      extensions: [
+        AppTheme(
+          colors: AppColors(
+            background: Color(0xFF607D8B),
+            surfaceContainer: Color(0xFFFFFFFF),
+            primary: Color(0xF8FFFFFF),
+            onSurfaceContainer: Color(0xFF000000),
+            error: Color(0xFFFF0000),
+            secondary: Color(0xFF0055FF),
+          ),
+        ),
+      ],
+    );
+  }
+
+  static ThemeData dark() {
+    return ThemeData(
+      brightness: Brightness.dark,
+      extensions: [
+        AppTheme(
+          colors: AppColors(
+            background:Color(0xFF323232),
+            surfaceContainer:Color(0xFF1E1E1E), 
+            primary: Color(0xFFF4F4F4),
+            onSurfaceContainer: Color(0xFFFFFFFF),
+            error: Color(0xFFFF6B6B),
+            secondary: Color(0xFF82B1FF),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/theme/styles/colors_and_styles.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:folk_robe/theme/app_theme.dart';
+
+class AppColors {
+  final Color? background;
+  final Color? surfaceContainer;
+  final Color? primary;
+  final Color? error;
+  final Color? onSurfaceContainer;
+  final Color? secondary;
+
+  AppColors({
+    this.background,
+    this.surfaceContainer,
+    this.primary,
+    this.error,
+    this.onSurfaceContainer,
+    this.secondary,
+  });
+
+  static AppColors lerp(AppColors a, AppColors b, double t) {
+    return AppColors(
+      background: Color.lerp(a.background, b.background, t),
+      primary: Color.lerp(a.primary, b.primary, t),
+      surfaceContainer: Color.lerp(a.surfaceContainer, b.surfaceContainer, t),
+      error: Color.lerp(a.error, b.error, t),
+      onSurfaceContainer:
+          Color.lerp(a.onSurfaceContainer, b.onSurfaceContainer, t),
+      secondary: Color.lerp(a.secondary, b.secondary, t),
+    );
+  }
+}
+
+class AppTextStyles {
+  final TextStyle titleMedium;
+  final TextStyle titleLarge;
+  final TextStyle bodyLarge;
+  final TextStyle labelMedium;
+
+  const AppTextStyles(
+      {this.titleMedium = const TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w200,
+      ),
+      this.titleLarge = const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+      ),
+      this.bodyLarge = const TextStyle(
+        fontSize: 16,
+        letterSpacing: 0.2,
+        fontWeight: FontWeight.w300,
+      ),
+      this.labelMedium = const TextStyle(
+        fontSize: 12,
+        letterSpacing: 0.1,
+        fontWeight: FontWeight.w400,
+      )});
+
+  AppTextStyles copyWith({
+    TextStyle? bodyLarge,
+    TextStyle? titleMedium,
+    TextStyle? titleLarge,
+    TextStyle? labelMedium,
+  }) {
+    return AppTextStyles(
+      bodyLarge: bodyLarge ?? this.bodyLarge,
+      titleMedium: titleMedium ?? this.titleMedium,
+      titleLarge: titleLarge ?? this.titleLarge,
+      labelMedium: labelMedium ?? this.labelMedium,
+    );
+  }
+
+  static AppTextStyles lerp(AppTextStyles a, AppTextStyles b, double t) =>
+      AppTextStyles();
+}
+
+extension AppThemeExtension on BuildContext {
+  AppTheme get appTheme {
+    final theme = Theme.of(this).extension<AppTheme>();
+
+    if (theme == null) {
+      throw Exception('AppTheme is not found in the theme context.');
+    }
+
+    return theme;
+  }
+}
