34b0a7c860ce03528bc1bf28d45ada3421d03164
Georgi Tasev <georgi.tasev12@gmail.com>
2025-08-25 20:57:17 +0300

    Update on common textfield, separate the input decoration styles into a boolean property, to decide if it's a search bar or a regular textfield. Then updated the common dialog.

--- lib/common/common_textfield.dart
+++ lib/common/common_textfield.dart
@@ -10,6 +10,10 @@ class CommonTextfield extends StatelessWidget {
   final TextInputType? keyboardType;
   final List<TextInputFormatter>? formatters;
   final bool isClearIconVisible;
+  final Icon? prefixIcon;
+  final IconButton? suffixIconButton;
+  final String? hintText;
+  final bool isSearchTextField;
 
   const CommonTextfield({
     super.key,
@@ -19,44 +23,78 @@ class CommonTextfield extends StatelessWidget {
     this.keyboardType,
     this.formatters,
     this.isClearIconVisible = false,
+    this.prefixIcon,
+    this.suffixIconButton,
+    this.hintText,
+    this.isSearchTextField = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-        onChanged: onChanged,
-        controller: textController,
-        textCapitalization: TextCapitalization.sentences,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              color: context.appTheme.colors.primary,
+      onChanged: onChanged,
+      controller: textController,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: !isSearchTextField
+          ? InputDecoration(
+              filled: true,
+              fillColor: context.appTheme.colors.surfaceContainer,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: context.appTheme.colors.primary),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(
+                    color: context.appTheme.colors.secondary, width: 2),
+              ),
+              suffixIcon: isClearIconVisible
+                  ? IconButton(
+                      onPressed: onIconButtonPress,
+                      icon: Icon(
+                        Icons.cancel_outlined,
+                        color: context.appTheme.colors.onSurfaceContainer,
+                      ),
+                    )
+                  : suffixIconButton,
+            )
+          : InputDecoration(
+              prefixIcon: prefixIcon,
+              hintText: hintText,
+              hintStyle: context.appTheme.textStyles.bodyLarge
+                  .copyWith(color: context.appTheme.colors.onSurfaceContainer),
+              suffixIcon: isClearIconVisible
+                  ? IconButton(
+                      onPressed: onIconButtonPress,
+                      icon: Icon(
+                        Icons.cancel_outlined,
+                        color: context.appTheme.colors.onSurfaceContainer,
+                      ),
+                    )
+                  : suffixIconButton,
+              contentPadding: EdgeInsets.only(top: 11),
+              filled: true,
+              fillColor:
+                  context.appTheme.colors.surfaceContainer.withAlpha(150),
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  width: 1,
+                  color: context.appTheme.colors.primary,
+                ),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  width: 2,
+                  color: context.appTheme.colors.primary,
+                ),
+              ),
             ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(
-              color: context.appTheme.colors.secondary,
-              width: 2,
-            ),
-          ),
-          suffixIcon: isClearIconVisible
-              ? IconButton(
-                  onPressed: onIconButtonPress,
-                  icon: Icon(
-                    Icons.cancel_outlined,
-                    color: context.appTheme.colors.onSurfaceContainer,
-                  ),
-                )
-              : null,
-        ),
-        cursorColor: context.appTheme.colors.secondary,
-        keyboardType: keyboardType ?? TextInputType.text,
-        inputFormatters: formatters ??
-            [
-              FilteringTextInputFormatter.allow(RegexHelper.wordsOnly),
-            ],
-      );
+      cursorColor: context.appTheme.colors.secondary,
+      keyboardType: keyboardType ?? TextInputType.text,
+      inputFormatters: formatters ??
+          [
+            FilteringTextInputFormatter.allow(RegexHelper.wordsOnly),
+          ],
+    );
   }
 }
