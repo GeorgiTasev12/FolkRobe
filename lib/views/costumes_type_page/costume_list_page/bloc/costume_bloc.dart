f97302967c5fe8cd33538e80f72b8be91ef35b7f
Georgi Tasev <georgi.tasev12@gmail.com>
2025-08-28 15:53:22 +0300

    Update on the Costumes bloc, added a DatabaseException to trigger database errors, while working with CRUD. And while moving the files, it updated the files to the new directory.

---
+++ lib/views/costumes_type_page/costume_list_page/bloc/costume_bloc.dart
@@ -0,0 +1,302 @@
+import 'dart:async';
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:folk_robe/dao/costume.dart';
+import 'package:folk_robe/models/options.dart';
+import 'package:folk_robe/models/status.dart';
+import 'package:folk_robe/repositories/costumes_repository.dart';
+import 'package:sqflite/sqflite.dart';
+
+part 'costume_event.dart';
+part 'costume_state.dart';
+
+class CostumeBloc extends Bloc<CostumeEvent, CostumeState> {
+  final Options selectedOption;
+  final GenderType genderType;
+
+  CostumeBloc({
+    required this.selectedOption,
+    required this.genderType,
+  }) : super(CostumeState(
+          nameTextController: TextEditingController(),
+          quantityTextController: TextEditingController(),
+          searchTextController: TextEditingController(),
+        )) {
+    on<InitDataEvent>(_onInitData);
+    on<AddCostumeEvent>(_onAddCostume);
+    on<UpdateCostumeEvent>(_onUpdateCostume);
+    on<RemoveCostumeEvent>(_onRemoveCostume);
+    on<OnNameChangedEvent>(_onNameChanged);
+    on<OnQuantityChangedEvent>(_onQuantityChanged);
+    on<OnNameClearEvent>(_onNameClear);
+    on<OnQuantityClearEvent>(_onQuantityClear);
+    on<OnCloseDialogEvent>(_onCloseDialog);
+    on<SearchCostumeEvent>(_onSearchCostume);
+    on<OnSearchClearEvent>(_onSearchClear);
+  }
+
+  @override
+  Future<void> close() {
+    state.nameTextController?.dispose();
+    state.quantityTextController?.dispose();
+    state.searchTextController?.dispose();
+    return super.close();
+  }
+
+  Future<void> _onInitData(
+    InitDataEvent event,
+    Emitter<CostumeState> emit,
+  ) async {
+    emit(state.copyWith(isLoading: true));
+
+    final costumes = await CostumesRepository().read(
+      option: selectedOption,
+      gender: genderType,
+    );
+
+    emit(state.copyWith(
+      allCostumesList: costumes,
+      costumeFiltered: costumes,
+      querySearch: "",
+      isLoading: false,
+    ));
+  }
+
+  Future<void> _onAddCostume(
+    AddCostumeEvent event,
+    Emitter<CostumeState> emit,
+  ) async {
+    try {
+      final costume = Costume(
+        title: event.title,
+        quantity: int.tryParse(event.quantity ?? ''),
+      );
+      final newId = await CostumesRepository().add(
+        item: costume,
+        gender: genderType,
+        option: selectedOption,
+      );
+
+      final costumeWithId = costume.copyWith(
+        id: newId,
+      );
+
+      emit(
+        state.copyWith(
+          costume: costumeWithId,
+          status: Status.success,
+          snackbarMessage: "Успешно сте добавили реквизит от костюма!",
+        ),
+      );
+    } on DatabaseException catch (dbError) {
+      emit(state.copyWith(
+        status: Status.error,
+        snackbarMessage: "Възникна грешка: ${dbError.toString()}",
+      ));
+      throw Exception(dbError);
+    } catch (e) {
+      emit(state.copyWith(
+        status: Status.error,
+        snackbarMessage: "Възникна грешка, моля опитайте по-късно.",
+      ));
+      throw Exception(e);
+    }
+
+    emit(state.copyWith(
+      status: Status.initial,
+      snackbarMessage: null,
+    ));
+  }
+
+  Future<void> _onRemoveCostume(
+    RemoveCostumeEvent event,
+    Emitter<CostumeState> emit,
+  ) async {
+    try {
+      await CostumesRepository().delete(
+        option: selectedOption,
+        id: event.id ?? 0,
+        gender: genderType,
+      );
+
+      final updatedList = await CostumesRepository().read(
+        option: selectedOption,
+        gender: genderType,
+      );
+
+      emit(state.copyWith(
+        allCostumesList: updatedList,
+        id: event.id,
+        status: Status.success,
+        snackbarMessage: "Успешно сте премахнали реквизита!",
+      ));
+    } on DatabaseException catch (dbError) {
+      emit(state.copyWith(
+        status: Status.error,
+        snackbarMessage: "Възникна грешка: ${dbError.toString()}",
+      ));
+      throw Exception(dbError);
+    } catch (e) {
+      emit(state.copyWith(
+        status: Status.error,
+        snackbarMessage: "Възникна грешка, моля опитайте по-късно.",
+      ));
+      throw Exception(e);
+    }
+
+    emit(state.copyWith(
+      status: Status.initial,
+      snackbarMessage: null,
+    ));
+  }
+
+  Future<void> _onUpdateCostume(
+    UpdateCostumeEvent event,
+    Emitter<CostumeState> emit,
+  ) async {
+    try {
+      final updatedCostume = Costume(
+        id: event.id,
+        title: event.title ?? '',
+        quantity: int.tryParse(event.quantity ?? ''),
+      );
+      await CostumesRepository().update(
+        id: event.id ?? 0,
+        option: selectedOption,
+        item: updatedCostume,
+        gender: genderType,
+      );
+
+      final updatedList = await CostumesRepository().read(
+        option: selectedOption,
+        gender: genderType,
+      );
+
+      emit(state.copyWith(
+        allCostumesList: updatedList,
+        costume: updatedCostume,
+        status: Status.success,
+        snackbarMessage: "Успешно сте променили реквизита!",
+      ));
+    } on DatabaseException catch (dbError) {
+      emit(state.copyWith(
+        status: Status.error,
+        snackbarMessage: "Възникна грешка: ${dbError.toString()}",
+      ));
+
+      throw Exception(dbError);
+    } catch (e) {
+      emit(state.copyWith(
+        status: Status.error,
+        snackbarMessage: "Възникна грешка, моля опитайте по-късно.",
+      ));
+
+      throw Exception(e);
+    }
+
+    emit(state.copyWith(
+      status: Status.initial,
+      snackbarMessage: null,
+    ));
+  }
+
+  void _onNameChanged(
+    OnNameChangedEvent event,
+    Emitter<CostumeState> emit,
+  ) {
+    emit(state.copyWith(
+      isNameNotEmpty: event.text.isNotEmpty ? true : false,
+    ));
+  }
+
+  void _onQuantityChanged(
+    OnQuantityChangedEvent event,
+    Emitter<CostumeState> emit,
+  ) {
+    emit(state.copyWith(
+      isQuantityNotEmpty: event.number.isNotEmpty ? true : false,
+    ));
+  }
+
+  void _onNameClear(
+    OnNameClearEvent event,
+    Emitter<CostumeState> emit,
+  ) {
+    final controller = event.textController;
+    controller.clear();
+
+    emit(state.copyWith(
+      nameTextController: controller,
+      isNameNotEmpty: false,
+    ));
+  }
+
+  void _onQuantityClear(
+    OnQuantityClearEvent event,
+    Emitter<CostumeState> emit,
+  ) {
+    final controller = event.textController;
+    controller.clear();
+
+    emit(
+      state.copyWith(
+        quantityTextController: controller,
+        isQuantityNotEmpty: false,
+      ),
+    );
+  }
+
+  void _onCloseDialog(
+    OnCloseDialogEvent event,
+    Emitter<CostumeState> emit,
+  ) {
+    state.nameTextController?.clear();
+    state.quantityTextController?.clear();
+
+    emit(state.copyWith(
+      isNameNotEmpty: false,
+      isQuantityNotEmpty: false,
+    ));
+  }
+
+  FutureOr<void> _onSearchCostume(
+    SearchCostumeEvent event,
+    Emitter<CostumeState> emit,
+  ) {
+    final query = event.query.trim().toLowerCase();
+
+    if (query.isEmpty) {
+      emit(state.copyWith(
+        costumeFiltered: null,
+        querySearch: '',
+      ));
+    }
+
+    final filtered = state.allCostumesList
+        ?.where((costume) => costume.title.toLowerCase().contains(query))
+        .toList();
+
+    emit(state.copyWith(
+      costumeFiltered: filtered,
+      querySearch: query,
+    ));
+  }
+
+  FutureOr<void> _onSearchClear(
+    OnSearchClearEvent event,
+    Emitter<CostumeState> emit,
+  ) {
+    final controller = event.textController;
+    controller.clear();
+
+    emit(state.copyWith(
+      costumeFiltered: null,
+      querySearch: '',
+      searchTextController: controller,
+    ));
+
+    add(InitDataEvent());
+  }
+}
---
+++ lib/views/costumes_type_page/costume_list_page/page.dart
@@ -0,0 +1,295 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:folk_robe/common/common_dialog.dart';
+import 'package:folk_robe/common/common_list_tile.dart';
+import 'package:folk_robe/common/common_snackbar.dart';
+import 'package:folk_robe/locator.dart';
+import 'package:folk_robe/models/options.dart';
+import 'package:folk_robe/models/status.dart';
+import 'package:folk_robe/service/navigation_service.dart';
+import 'package:folk_robe/theme/styles/colors_and_styles.dart';
+import 'package:folk_robe/views/core_page.dart';
+import 'package:folk_robe/views/costumes_type_page/costume_list_page/bloc/costume_bloc.dart';
+import 'package:folk_robe/common/common_delete_dialog.dart';
+import 'package:folk_robe/common/common_empty_info_text.dart';
+
+class CostumeListPage extends HookWidget {
+  const CostumeListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.read<CostumeBloc>();
+
+    useEffect(() {
+      bloc.add(InitDataEvent(options: Options.shopska));
+      return null;
+    }, const []);
+
+    return BlocListener<CostumeBloc, CostumeState>(
+      listenWhen: (previous, current) =>
+          previous.status != current.status ||
+          previous.snackbarMessage != current.snackbarMessage,
+      listener: (context, state) {
+        if (state.status == Status.success || state.status == Status.error) {
+          showCommonSnackbar(
+            context: context,
+            message: state.snackbarMessage,
+            status: state.status,
+          );
+        }
+      },
+      child: BlocBuilder<CostumeBloc, CostumeState>(
+        buildWhen: (previous, current) =>
+            previous.id != current.id ||
+            previous.allCostumesList != current.allCostumesList ||
+            previous.costumeFiltered != current.costumeFiltered ||
+            previous.searchTextController != current.searchTextController,
+        builder: (context, state) {
+          return CorePage(
+            hasFAB: true,
+            hasAppBarTitle: true,
+            appBarTitle: 'Костюми',
+            onFABPressed: () => showDialog(
+              context: context,
+              builder: (context) {
+                return BlocProvider.value(
+                  value: bloc,
+                  child: BlocBuilder<CostumeBloc, CostumeState>(
+                    buildWhen: (previous, current) =>
+                        previous.nameTextController !=
+                            current.nameTextController ||
+                        previous.quantityTextController !=
+                            current.quantityTextController ||
+                        previous.isNameNotEmpty != current.isNameNotEmpty ||
+                        previous.isQuantityNotEmpty !=
+                            current.isQuantityNotEmpty,
+                    builder: (context, state) {
+                      return CommonDialog(
+                        dialogTitle: 'Моля, въведете реквизит.',
+                        onSavePressed: () {
+                          bloc.add(AddCostumeEvent(
+                            title: state.nameTextController?.text ?? "",
+                            quantity: state.quantityTextController?.text,
+                          ));
+                          bloc.add(InitDataEvent());
+                          locator<NavigationService>().pop();
+                        },
+                        onClosedPressed: () {
+                          bloc.add(OnCloseDialogEvent());
+                          locator<NavigationService>().pop();
+                        },
+                        onNameClearPressed: () => bloc.add(
+                          OnNameClearEvent(
+                            textController: state.nameTextController ??
+                                TextEditingController(),
+                          ),
+                        ),
+                        onQuantityClearPressed: () => bloc.add(
+                          OnQuantityClearEvent(
+                            textController: state.quantityTextController ??
+                                TextEditingController(),
+                          ),
+                        ),
+                        onNameChanged: (String name) => bloc.add(
+                          OnNameChangedEvent(text: name),
+                        ),
+                        onNumberChanged: (String number) => bloc.add(
+                          OnQuantityChangedEvent(number: number),
+                        ),
+                        nameTextController:
+                            state.nameTextController ?? TextEditingController(),
+                        quantityTextController: state.quantityTextController ??
+                            TextEditingController(),
+                        isNameNotEmpty: state.isNameNotEmpty,
+                        isQuantityNotEmpty: state.isQuantityNotEmpty,
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+            hasSearchBar: true,
+            onSearchChanged: (value) =>
+                bloc.add(SearchCostumeEvent(query: value)),
+            searchTextController: state.searchTextController,
+            isSuffixIconVisible: state.querySearch?.isNotEmpty,
+            onSuffixPressed: () => bloc.add(
+              OnSearchClearEvent(
+                textController:
+                    state.searchTextController ?? TextEditingController(),
+              ),
+            ),
+            child: state.allCostumesList?.isEmpty ?? false
+                ? CommonEmptyInfoText(isDancer: false)
+                : BlocBuilder<CostumeBloc, CostumeState>(
+                    bloc: bloc,
+                    buildWhen: (previous, current) =>
+                        previous.allCostumesList != current.allCostumesList ||
+                        previous.id != current.id ||
+                        previous.costumeFiltered != current.costumeFiltered ||
+                        previous.isLoading != current.isLoading,
+                    builder: (context, state) {
+                      final displayList =
+                          state.costumeFiltered ?? state.allCostumesList ?? [];
+
+                      if (state.isLoading) {
+                        return Center(
+                          child: CircularProgressIndicator(
+                            color: context.appTheme.colors.primary,
+                          ),
+                        );
+                      }
+
+                      if (displayList.isEmpty) {
+                        return Center(
+                          child: Text(
+                            'Няма резултати..',
+                            style: context.appTheme.textStyles.titleMedium
+                                .copyWith(
+                              color: context.appTheme.colors.primary,
+                            ),
+                          ),
+                        );
+                      }
+
+                      return ListView.separated(
+                        itemCount: displayList.length,
+                        separatorBuilder: (_, __) => const SizedBox(height: 15),
+                        itemBuilder: (context, index) {
+                          final costume = displayList[index];
+
+                          return CommonListTile(
+                            title: costume.title,
+                            quantity: (costume.quantity != null)
+                                ? costume.quantity.toString()
+                                : null,
+                            suffixWidgets: [
+                              ClipOval(
+                                child: Material(
+                                  color: context.appTheme.colors.warning,
+                                  child: IconButton(
+                                    onPressed: () => showDialog(
+                                      context: context,
+                                      builder: (_) => BlocProvider.value(
+                                        value: bloc,
+                                        child: CommonDialog(
+                                          dialogTitle:
+                                              'Моля, въведете реквизит.',
+                                          onSavePressed: () {
+                                            bloc.add(UpdateCostumeEvent(
+                                              id: costume.id,
+                                              title: state.nameTextController
+                                                      ?.text ??
+                                                  "",
+                                              quantity: state
+                                                  .quantityTextController?.text,
+                                            ));
+                                            bloc.add(InitDataEvent());
+                                            locator<NavigationService>().pop();
+                                          },
+                                          onClosedPressed: () {
+                                            bloc.add(OnCloseDialogEvent());
+                                            locator<NavigationService>().pop();
+                                          },
+                                          onNameClearPressed: () =>
+                                              bloc.add(OnNameClearEvent(
+                                            textController:
+                                                state.nameTextController ??
+                                                    TextEditingController(),
+                                          )),
+                                          onQuantityClearPressed: () =>
+                                              bloc.add(
+                                            OnQuantityClearEvent(
+                                              textController: state
+                                                      .quantityTextController ??
+                                                  TextEditingController(),
+                                            ),
+                                          ),
+                                          onNameChanged: (String name) =>
+                                              bloc.add(
+                                            OnNameChangedEvent(text: name),
+                                          ),
+                                          onNumberChanged: (String number) =>
+                                              bloc.add(
+                                            OnQuantityChangedEvent(
+                                                number: number),
+                                          ),
+                                          isNameNotEmpty: state.isNameNotEmpty,
+                                          isQuantityNotEmpty:
+                                              state.isQuantityNotEmpty,
+                                          nameTextController:
+                                              state.nameTextController ??
+                                                  TextEditingController(),
+                                          quantityTextController:
+                                              state.quantityTextController,
+                                        ),
+                                      ),
+                                    ),
+                                    icon: Icon(
+                                      Icons.edit,
+                                      color: context
+                                          .appTheme.colors.surfaceContainer,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 16),
+                              ClipOval(
+                                child: Material(
+                                  color: context.appTheme.colors.error,
+                                  child: IconButton(
+                                    onPressed: () => showDialog(
+                                      context: context,
+                                      builder: (context) => BlocProvider.value(
+                                        value: bloc,
+                                        child: BlocBuilder<CostumeBloc,
+                                            CostumeState>(
+                                          buildWhen: (previous, current) =>
+                                              previous.id != current.id ||
+                                              previous.allCostumesList !=
+                                                  current.allCostumesList ||
+                                              previous.costumeFiltered !=
+                                                  current.costumeFiltered,
+                                          builder: (context, state) {
+                                            if (state.allCostumesList == null ||
+                                                index >=
+                                                    state.allCostumesList!
+                                                        .length) {
+                                              return const SizedBox.shrink();
+                                            }
+                                            return CommonDeleteDialog(
+                                              index: costume.id ?? 0,
+                                              onDeletePressed: () {
+                                                bloc.add(RemoveCostumeEvent(
+                                                  id: costume.id ?? 0,
+                                                ));
+                                                bloc.add(InitDataEvent());
+                                                locator<NavigationService>()
+                                                    .pop();
+                                              },
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                    icon: Icon(
+                                      Icons.delete,
+                                      color: context
+                                          .appTheme.colors.surfaceContainer,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+          );
+        },
+      ),
+    );
+  }
+}
